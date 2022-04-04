924859d5199f90aa36bcc82726cf41d1470e7b20
rabbit-22 <chlwlal20@gmail.com>
2022-04-05 01:44:07 +0900

    네이버 지도 API 연동

--- app/src/main/java/kr/ac/tukorea/foodmap/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/foodmap/MainActivity.kt
@@ -1,11 +1,75 @@
 package kr.ac.tukorea.foodmap
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Color
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.UiThread
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.location.*
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
+    val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+    var permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fm: FragmentManager = supportFragmentManager
+        var mapFragment: MapFragment? = fm.findFragmentById(R.id.map) as MapFragment?
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance()
+            fm.beginTransaction().add(R.id.map, mapFragment).commit()
+        }
+        mapFragment!!.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
     }
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap!!.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE)
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+        uiSettings.isZoomGesturesEnabled = true
+        uiSettings.isScrollGesturesEnabled = true
+        uiSettings.isTiltGesturesEnabled = false
+        uiSettings.isRotateGesturesEnabled = false
+
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.size > 0
+                && grantResults[0] == PERMISSION_GRANTED
+            ) {
+                naverMap!!.locationTrackingMode = LocationTrackingMode.Follow
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
 }
