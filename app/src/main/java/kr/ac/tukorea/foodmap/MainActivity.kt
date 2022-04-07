e350c1797a5cc85e7b1b7d8a05d9a490e051a24a
rabbit-22 <chlwlal20@gmail.com>
2022-04-07 15:56:43 +0900

    navigation drawer 생성

--- app/src/main/java/kr/ac/tukorea/foodmap/MainActivity.kt
+++ app/src/main/java/kr/ac/tukorea/foodmap/MainActivity.kt
@@ -1,29 +1,46 @@
 package kr.ac.tukorea.foodmap
 
 import android.Manifest
+import androidx.appcompat.widget.Toolbar
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
 import com.naver.maps.map.*
 import com.naver.maps.map.util.FusedLocationSource
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : FragmentActivity(), OnMapReadyCallback {
+    class MainActivity : AppCompatActivity(), OnMapReadyCallback,  NavigationView.OnNavigationItemSelectedListener  {
     val LOCATION_PERMISSION_REQUEST_CODE = 1000
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
-    private val listTab by lazy { TabListFragment() }
-    private val mapTab by lazy { TabMapFragment() }
-    private val settingTab by lazy { TabSettingFragment() }
+    lateinit var navigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
+
 //    val db = AppDatabase.getInstance(applicationContext)
     var permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar) // toolBar를 통해 App Bar 생성
+        setSupportActionBar(toolbar) // 툴바 적용
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로어를 꺼낼 홈 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24) // 홈버튼 이미지 변경
+        supportActionBar?.setDisplayShowTitleEnabled(false) // 툴바에 타이틀 안보이게
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
         val fm: FragmentManager = supportFragmentManager
         var mapFragment: MapFragment? = fm.findFragmentById(R.id.map) as MapFragment?
         if (mapFragment == null) {
@@ -32,33 +49,7 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         }
         mapFragment!!.getMapAsync(this)
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-        initNavigationBar()
     }
-    private fun initNavigationBar() {
-        bottomNavigationView.run {
-            setOnItemSelectedListener {
-                when(it.itemId) {
-                R.id.tab_map -> { changeFragment(mapTab)
-            }
-                R.id.tab_list -> { changeFragment(listTab)
-                }
-                R.id.tab_setting -> { changeFragment(settingTab)
-                }
-            }
-                true
-            }
-        }
-    }
-
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.home_ly, fragment)
-            .commit()
-    }
-
-
-
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
         naverMap!!.locationSource = locationSource
@@ -72,8 +63,6 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         uiSettings.isRotateGesturesEnabled = false
 
     }
-
-
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>,
                                             grantResults: IntArray) {
@@ -86,5 +75,37 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            android.R.id.home->{
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
+
+        when(item.itemId){
+
+            R.id.tab_map->
+                Toast.makeText(this,"menu_item1 실행",Toast.LENGTH_SHORT).show()
+            R.id.tab_list->
+                Toast.makeText(this,"menu_item2 실행",Toast.LENGTH_SHORT).show()
+            R.id.tab_setting-> null
+
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return false
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
 }
